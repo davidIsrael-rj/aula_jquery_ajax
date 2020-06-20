b76ec8c2f36aa26d78d973761990fdb7a875bbd7
David Israel <davidisrael.rj@gmail.com>
2020-06-20 12:32:16 -0300

    Foi adicionado condições para verificação do CEP, se foi digitado letras, mais ou menos de 8 dígitos e se o CEP foi encontrado.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,106 @@
 function consultaCep(){
     $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
-    var url = "https://viacep.com.br/ws/" + cep + "/json/";
-    $.ajax({
-        url:url,
-        type: "GET", 
-        success: function(response){
-            console.log(response);
-            console.log("Segundo: "+response.bairro);
-            console.log("Terceiro: "+response.cep);
-            
-            $("#logradouro").html(response.logradouro);
-
-            $("#bairro").html(response.bairro);
-
-            $("#localidade").html(response.localidade);
-
-            $("#uf").html(response.uf);
-
-            $("#titulo_cep").html("CEP: "+response.cep);
-
-            $(".cep").show();
+     //Verificando se a variavel cep tem somente numeros    
+     if(!isNaN(cep)){
+        
+        //verificando de a variavel tem 8 caracteres
+        if ((cep.length < 8) || (cep.length > 8) ){
+        
+            $("#titulo_cep").html("DIGITE UM CEP VALIDO<br>POR FAVOR");
+                    
+            $("#logradouro").html(" ");
+    
+            $("#bairro").html(" ");
+    
+            $("#localidade").html(" ");
+    
+            $("#uf").html("");
+    
             $(".barra-progresso").hide();
+    
+            $(".cep").show();
+    
+        }else{
+            var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    
+            console.log(cep);
+            console.log(url);
+        
+            $.ajax({
+                url:url,
+                type: "GET", 
+                success: function(response){
+                    console.log(response);
+                    console.log("Segundo: "+response.bairro);
+                    console.log("Terceiro: "+response.cep);
+                    
+                    //verifica se o cep é valido
+                    if(response.erro){
+    
+                        $("#titulo_cep").html("CEP: "+ cep +" NÃO ENCONTRADO");
+                        
+                        $("#logradouro").html(" ");
+    
+                        $("#bairro").html(" ");
+    
+                        $("#localidade").html(" ");
+    
+                        $("#uf").html("");
+    
+                        $(".barra-progresso").hide();
+    
+                        $(".cep").show();
+    
+                        
+                    }else{
+                    $("#logradouro").html(response.logradouro);
+    
+                        $("#bairro").html(response.bairro);
+    
+                        $("#localidade").html(response.localidade);
+    
+                        $("#uf").html(response.uf);
+    
+                        $("#titulo_cep").html("CEP: "+response.cep);
+    
+                        $(".cep").show();
+                        $(".barra-progresso").hide();
+                    }
+                    /*
+                    document.getElementById("logradouro").innerHTML = response.logradouro;
+    
+                    document.getElementById("bairro").innerHTML = response.bairro;
+    
+                    document.getElementById("localidade").innerHTML = response.localidade;
+    
+                    document.getElementById("uf").innerHTML = response.uf;
+                    */
+                }
+            });
         }
-        /*
-        document.getElementById("logradouro").innerHTML = response.logradouro;
+    }else{
 
-        document.getElementById("bairro").innerHTML = response.bairro;
+        $("#titulo_cep").html("POR FAVOR, DIGITE SOMENTE NUMEROS");
+                    
+        $("#logradouro").html(" ");
 
-        document.getElementById("localidade").innerHTML = response.localidade;
+        $("#bairro").html(" ");
 
-        document.getElementById("uf").innerHTML = response.uf;
-        */
-    });
-};
+        $("#localidade").html(" ");
 
+        $("#uf").html("");
+
+        $(".barra-progresso").hide();
+
+        $(".cep").show();
+    }
+    
+    
+
+
+}
 $(function(){
     $(".cep").hide();
     $(".barra-progresso").hide();
-});
+})
